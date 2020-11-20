235e3bb3880575e433a75d3d36946b54645decb7
WilliamSaya-lvl30 <williams.sand25@gmail.com>
2020-11-20 19:03:47 -0300

    layaut vistas frelance

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import { Route, Switch,Link } from 'react-router-dom'
+import { Route, Switch,Link,Redirect } from 'react-router-dom'
 import RegisterFreelancerContainer from './containers/RegisterFreelancerContainer'
 import React, { useEffect, useState } from "react";
 import { hot } from "react-hot-loader/root";
@@ -9,6 +8,7 @@ import { db } from "../firebase/firebase"
 import LoginContainer from './containers/loginContainer'
 import { AuthProvider } from '../auth/auth'
 import { Layout, Menu, Typography } from 'antd';
+import UserContainer from './containers/userContainer';
 
 const { Header, Content, Footer } = Layout;
 const {Title} =Typography
@@ -19,27 +19,15 @@ function App() {
   return (
 
     <AuthProvider>
-      <Layout className="layout">
-        <Header>
-          <div className="logo"><Title level={3} className='titulo' >ITESA</Title></div>
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} className='navbar'>
-            <Menu.Item key="1"><Link to='/home'>Home</Link></Menu.Item>
-            <Menu.Item key="2"><Link to='/register'>Register</Link></Menu.Item>
-            <Menu.Item key="3"><Link to='/login'>Login</Link></Menu.Item>
-            <Menu.Item key="4"><Link to='/admin'>Admin</Link></Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <div className="site-layout-content">
-            <Switch>
-                <Route exact path='/register' component={RegisterFreelancerContainer} />
-                <Route exact path='/login' component={LoginContainer} />
-            </Switch>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Talentos ITESA ©2020 Created by Plataforma 5</Footer>
-      </Layout>
-            
+    
+    <Switch>
+        <Route exact path='/register' component={RegisterFreelancerContainer} />
+        <Route exact path='/login' component={LoginContainer} />
+        <Route exact path='/frelance' component={UserContainer} />
+        <Route path="/" component={RegisterFreelancerContainer}/>
+        <Redirect from="/" to="/" />
+    </Switch>
+        
     </AuthProvider>
 
   )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./styles.css";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter, Route } from 'react-router-dom'
 import 'antd/dist/antd.css'
