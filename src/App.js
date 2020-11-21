95d6f717147015bb918e0da5bb890beea1568f20
Bruno Del Piero <bdelpiero93@gmail.com>
2020-11-21 10:23:46 -0300

    sube pdf como blob al Storage

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
-import { Route, Switch,Link,Redirect } from 'react-router-dom'
-import RegisterFreelancerContainer from './containers/RegisterFreelancerContainer'
+import { Route, Switch, Link, Redirect } from "react-router-dom";
+import RegisterFreelancerContainer from "./containers/RegisterFreelancerContainer";
 import React, { useEffect, useState } from "react";
 import { hot } from "react-hot-loader/root";
 import { useRecoilState } from "recoil";
 import { userName } from "./atoms";
-import { db } from "../firebase/firebase"
-import LoginContainer from './containers/loginContainer'
-import { AuthProvider } from '../auth/auth'
-import { Layout, Menu, Typography } from 'antd';
-import UserContainer from './containers/userContainer';
-import AdminContainer from './containers/adminContainer';
-
+import { db } from "../firebase/firebase";
+import LoginContainer from "./containers/loginContainer";
+import { AuthProvider } from "../auth/auth";
+import { Layout, Menu, Typography } from "antd";
+import UserContainer from "./containers/userContainer";
+import AdminContainer from "./containers/adminContainer";
 
 const { Header, Content, Footer } = Layout;
-const {Title} =Typography
-
+const { Title } = Typography;
 
 function App() {
-
   return (
-
     <AuthProvider>
-    
-    <Switch>
+      <Switch>
         <Route exact path='/register' component={RegisterFreelancerContainer} />
         <Route exact path='/login' component={LoginContainer} />
-        <Route exact path='/frelance' component={UserContainer} />
+        <Route exact path='/freelancer' component={UserContainer} />
         <Route exact path='/admin' component={AdminContainer} />
-        <Route path="/" component={RegisterFreelancerContainer}/>
-        <Redirect from="/" to="/" />
-    </Switch>
-        
+        <Route path='/' component={RegisterFreelancerContainer} />
+        <Redirect from='/' to='/' />
+      </Switch>
     </AuthProvider>
-
-  )
+  );
 }
 
 export default App;
--- src/containers/RegisterFreelancerContainer.js
+++ src/containers/RegisterFreelancerContainer.js
@@ -1,78 +1,103 @@
 import React, { useState } from "react";
-import RegisterFreelancer from '../components/RegisterFreelancer'
-import { db } from '../../firebase/firebase'
-import SignatureCanvas from 'react-signature-canvas'
+import RegisterFreelancer from "../components/RegisterFreelancer";
+import { db } from "../../firebase/firebase";
+import SignatureCanvas from "react-signature-canvas";
+import html2pdf from "html2pdf.js";
+import { storage } from "../../firebase/firebase";
 
 function RegisterFreelancerContainer() {
+  const [data, setData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+    freelancerType: "",
+  });
 
-    const [data, setData] = useState({
-        name: '',
-        lastName: '',
-        email: '',
-        password: '',
-        freelancerType: '',
-    })
+  const [bankData, setBankData] = useState({
+    bankName: "",
+    accountName: "",
+    alias: "",
+    cbu: "",
+    dni: "",
+  });
 
-    const [bankData, setBankData] = useState({
-        bankName: '',
-        accountName: '',
-        alias: '',
-        cbu: '',
-        dni: ''
-    })
+  const [step, setStep] = useState(1);
 
-    const [step, setStep] = useState(1)
+  const handleChange = (e) => {
+    if (step == 1)
+      setData({
+        ...data,
+        [e.target.name]: e.target.value,
+      });
+    if (step == 2)
+      setBankData({
+        ...bankData,
+        [e.target.name]: e.target.value,
+      });
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("ESTO ES DATA", data);
+    if (step < 3) setStep(step + 1);
+    const element = document.getElementById("to-print");
 
-    const handleChange = (e) => {
-        if (step == 1) setData({
-            ...data, [e.target.name]: e.target.value
-        })
-        if (step == 2) setBankData({
-            ...bankData, [e.target.name]: e.target.value
-        })
-    }
+    const worker = html2pdf()
+      .from(element)
+      .toPdf()
+      .output("blob", "signature.pdf");
+    worker.then((file) => {
+      const storageRef = storage.ref();
+      const pdfsRef = storageRef.child("pdfs/contract.pdf");
+      pdfsRef.put(file).then(function (snapshot) {
+        console.log("Uploaded a blob or file!");
+      });
+    });
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log('ESTO ES DATA', data)
-        if (step < 3) setStep(step + 1)
-        else (
-            data.bankDetails = bankData,
-            db.collection('users').add(data),
-            setData({
-                name: '',
-                lastName: '',
-                email: '',
-                password: '',
-                freelancerType: ''
-            }),
-            setBankData({
-                bankName: '',
-                accountName: '',
-                alias: '',
-                cbu: '',
-                dni: ''
-            })
-        )
-    }
+    // const storageRef = storage.ref();
+    // const pdfsRef = storageRef.child("pdfs");
+    // pdfsRef.put(pdfFile).then(function (snapshot) {
+    //   console.log("Uploaded a blob or file!");
+    // });
+    // else (
+    //     data.bankDetails = bankData,
+    //     db.collection('users').add(data),
+    //     setData({
+    //         name: '',
+    //         lastName: '',
+    //         email: '',
+    //         password: '',
+    //         freelancerType: ''
+    //     }),
+    //     setBankData({
+    //         bankName: '',
+    //         accountName: '',
+    //         alias: '',
+    //         cbu: '',
+    //         dni: ''
+    //     })
+    // )
+  };
 
-    return (
-        <div>
-            <div style={{ width: 500, border: '1px solid black' }}>
-                <SignatureCanvas penColor='green'
-                    canvasProps={{ width: 500, height: 200, className: 'sigCanvas' }} />
-            </div>
+  return (
+    <div id='to-print'>
+      <div style={{ width: 500, border: "1px solid black" }}>
+        <SignatureCanvas
+          penColor='green'
+          canvasProps={{ width: 500, height: 200, className: "sigCanvas" }}
+        />
+      </div>
 
-            <RegisterFreelancer
-                handleChange={handleChange}
-                handleSubmit={handleSubmit}
-                data={data}
-                bankData={bankData}
-                step={step} />
-        </div>
-
-    );
+      <RegisterFreelancer
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        data={data}
+        bankData={bankData}
+        step={step}
+      />
+    </div>
+  );
 }
 
 export default RegisterFreelancerContainer;
