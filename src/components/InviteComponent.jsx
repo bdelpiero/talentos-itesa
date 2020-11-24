b487ecc025f02e91a93ef875c59331b286775717
millasmar <marcosmillas@gmail.com>
2020-11-24 11:04:36 -0300

    Agregado de modales Admin, Invitar usuarios

--- src/components/InviteComponent.jsx
+++ src/components/InviteComponent.jsx
@@ -1,12 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
-function InviteComponent({ handleClick }) {
+import { db } from "../../firebase/firebase";
+import { authUser } from "../../auth/auth";
+
+import {
+  CheckCircleOutlined,
+  CloseCircleOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Modal, Button } from "antd";
+
+function InviteComponent() {
+  const { signup } = authUser();
+  const [modal, setModal] = useState(false);
+  const [email, setEmail] = useState("");
+  const openModal = () => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  const invite = () => {
+    closeModal();
+    openModalOk();
+  };
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  function success() {
+    closeModal();
+    db.collection("invites")
+      .add({
+        email,
+      })
+      .then(() => {
+        Modal.success({
+          bodyStyle: {
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "center",
+          },
+          content: "Solicitud Enviada!",
+          centered: "true",
+          okText: "VOLVER",
+          icon: <CheckCircleOutlined style={{ color: "#9e39ff" }} />,
+          okButtonProps: {
+            style: {
+              backgroundColor: "#9e39ff",
+              border: "none",
+              borderRadius: "10px",
+            },
+          },
+        });
+      });
+  }
   return (
-    <div>
-      <form action="">
-        <input type="text" />
-        <button onClick={handleClick}>Enviar</button>
-      </form>
+    <div className="Modal">
+      <Button type="primary" onClick={openModal}>
+        <UserOutlined twoToneColor="#9e39ff" />
+      </Button>
+      <Modal
+        title="Modal invite"
+        visible={modal}
+        okText="Invitar"
+        centered="true"
+        cancelButtonProps={{ hidden: true }}
+        okButtonProps={{
+          style: { backgroundColor: "#9e39ff", border: "none" },
+          shape: "round",
+        }}
+        onCancel={closeModal}
+        onOk={success}
+        closeIcon={<CloseCircleOutlined style={{ color: "#9e39ff" }} />}
+        bodyStyle={{ color: "#9e39ff" }}
+      >
+        <h2>Invitar Usuario</h2>
+        <form action="">
+          <input onChange={handleChange} type="text" />
+        </form>
+      </Modal>
     </div>
   );
 }
--- src/containers/AdminContainer.jsx
+++ src/containers/AdminContainer.jsx
@@ -1,86 +1,39 @@
 import React, { useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
-import { userAdmin } from "../atoms";
-import { db } from "../../firebase/firebase";
-import { authUser } from "../../auth/auth";
+import { Route, Link, useHistory } from "react-router-dom";
 import InviteComponent from "../components/InviteComponent";
-import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
-import { Modal, Button } from "antd";
-import Form from "antd/lib/form/Form";
+import { Layout, Row } from "antd";
+import Sidebar from "../components/sidebar";
+import { authUser } from "../../auth/auth";
+import HeaderComponent from "../components/header";
+
 function AdminContainer() {
-  const { signup } = authUser();
-  const [modal, setModal] = useState(false);
-  const [email, setEmail] = useState("");
-  const openModal = () => {
-    setModal(true);
-  };
+  const { Header, Footer, Sider, Content } = Layout;
+  const { logout, currentUser } = authUser();
+  const history = useHistory();
 
-  const closeModal = () => {
-    setModal(false);
+  const handleLogout = () => {
+    logout();
+    history.push("/");
   };
-
-  const invite = () => {
-    closeModal();
-    openModalOk();
-  };
-  const handleChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  function success() {
-    closeModal();
-    db.collection("invites")
-      .add({
-        email,
-      })
-      .then(() => {
-        Modal.success({
-          bodyStyle: {
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-            justifyContent: "center",
-          },
-          content: "Solicitud Enviada!",
-          centered: "true",
-          okText: "VOLVER",
-          icon: <CheckCircleOutlined style={{ color: "#9e39ff" }} />,
-          okButtonProps: {
-            style: {
-              backgroundColor: "#9e39ff",
-              border: "none",
-              borderRadius: "10px",
-            },
-          },
-        });
-      });
-  }
+  console.log("userContainer", currentUser);
   return (
-    <div className="Modal">
-      <Button type="primary" onClick={openModal}>
-        Invitar USUARUI LOGO
-      </Button>
-      <Modal
-        title="Modal invite"
-        visible={modal}
-        okText="Invitar"
-        centered="true"
-        cancelButtonProps={{ hidden: true }}
-        okButtonProps={{
-          style: { backgroundColor: "#9e39ff", border: "none" },
-          shape: "round",
-        }}
-        onCancel={closeModal}
-        onOk={success}
-        closeIcon={<CloseCircleOutlined style={{ color: "#9e39ff" }} />}
-        bodyStyle={{ color: "#9e39ff" }}
-      >
-        <h2>Invitar Usuario</h2>
-        <form action="">
-          <input onChange={handleChange} type="text" />
-        </form>
-      </Modal>
-    </div>
+    <Layout>
+      <Sider className="sider-user" justify="center">
+        <Sidebar handleLogout={handleLogout} />
+      </Sider>
+      <Layout>
+        <Header className="header-user">
+          <HeaderComponent />
+        </Header>
+        <Content className="content-user">
+          <Row className="content-row">
+            ofertas / proximo pago / mi banco <InviteComponent />
+          </Row>
+
+          <Row className="content-row">pagos</Row>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
